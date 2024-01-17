6d0cea9cf461879e63d4205d07ee3e577283b794
Karina <karinakios@mail.ru>
2024-01-17 16:08:48 +0300

    17.01.2024

--- src/ConnectionManager.java
+++ src/ConnectionManager.java
@@ -4,16 +4,19 @@ import java.sql.SQLException;
 
 public final class ConnectionManager {
 
-    private static final String PASSWORD = "postgres";
-    private static final String USERNAME = "postgres";
-    private static final String URL = "jdbc:postgresql://localhost:5432/flight_repository";
+    private static final String PASSWORD_KEY = "db.password";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String URL_KEY = "db.url";
 
     private ConnectionManager(){
 
     }
     public static Connection open(){
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY));
         } catch (SQLException e){
             throw new RuntimeException(e);
         }
